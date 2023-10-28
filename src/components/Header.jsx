3e865789bd87307794421b155c6479b61a0e09b4
Ali Yolalan <aliyolalan@outlook.com>
2023-10-29 02:23:59 +0300

    feat(STYLE-103): Country detail page style.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   const [theme, setTheme] = useState("Dark Theme");
   const onThemeChangeHandler = () => {
-    const newThemeSetting = theme === "Dark Theme" ? "Light Theme" : "Dark Theme"
-    setTheme(newThemeSetting)
-  }
+    const newThemeSetting =
+      theme === "Dark Theme" ? "Light Theme" : "Dark Theme";
+    setTheme(newThemeSetting);
+  };
 
   return (
-    <div className="bg-[#2B3743] h-20 text-white mb-[49px]">
+    <div className="bg-[#2B3743] h-20 text-white mb-[50px]">
       <div className="container h-20 mx-auto flex justify-between items-center">
         <h2 className="text-2xl">
-          Where in the world?
+          <Link to="/countries">Where in the world?</Link>
         </h2>
         <div className="cursor-pointer" onClick={onThemeChangeHandler}>
           {theme}
